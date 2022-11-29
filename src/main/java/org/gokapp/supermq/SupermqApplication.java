48b9781917d2eab4063a595cf1a50a15aa96df20
Gokul Rangarajan <gokulraja2006@gmail.com>
2022-11-29 22:59:50 +0530

    Initial project files

---
+++ src/main/java/org/gokapp/supermq/SupermqApplication.java
@@ -0,0 +1,13 @@
+package org.gokapp.supermq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermqApplication.class, args);
+	}
+
+}
